b6d233710a4ccd70d00c2c862d4303ccc0d8bd39
dr1co <dricomatoba@hotmail.com>
2023-10-01 10:57:11 -0300

    [feat]: ajustes de nome e confirmação

--- script.js
+++ script.js
@@ -3,92 +3,84 @@ let bebida, precoBebida, stringBebida
 let sobremesa, precoSobremesa, stringSobremesa
 let precoTotal
 
-function selecionarPrato(elemento)
-{
-    const checkPar = document.querySelector(".opcoes-prato").querySelector(".exibir")
+function selecionarPrato(elemento) {
+  const checkPar = document.querySelector(".opcoes-prato").querySelector(".exibir")
 
-    if(prato != null)
-    {
-        prato.classList.toggle("selecionado")
-        checkPar.classList.toggle("exibir")
-    }
+  if (prato != null) {
+    prato.classList.toggle("selecionado")
+    checkPar.classList.toggle("exibir")
+  }
 
-    elemento.classList.toggle("selecionado")
-    const checkmark = elemento.querySelector(".icone")
-    checkmark.classList.toggle("exibir")
-    prato = document.querySelector(".opcoes-prato").querySelector(".selecionado")
-    stringPrato = prato.querySelector("h3").innerHTML
-    precoPrato = prato.querySelector("h2").innerHTML
-    confirmacao()
+  elemento.classList.toggle("selecionado")
+  const checkmark = elemento.querySelector(".icone")
+  checkmark.classList.toggle("exibir")
+  prato = document.querySelector(".opcoes-prato").querySelector(".selecionado")
+  stringPrato = prato.querySelector("h3").innerHTML
+  precoPrato = prato.querySelector("h2").innerHTML
+  confirmacao()
 }
 
-function selecionarBebida(elemento)
-{
-    const checkPar = document.querySelector(".opcoes-bebida").querySelector(".exibir")
-    
-    if(bebida != null)
-    {
-        bebida.classList.toggle("selecionado")
-        checkPar.classList.toggle("exibir")
-    }
+function selecionarBebida(elemento) {
+  const checkPar = document.querySelector(".opcoes-bebida").querySelector(".exibir")
 
-    elemento.classList.toggle("selecionado")
-    const checkmark = elemento.querySelector(".icone")
-    checkmark.classList.toggle("exibir")
-    bebida = document.querySelector(".opcoes-bebida").querySelector(".selecionado")
-    stringBebida = bebida.querySelector("h3").innerHTML
-    precoBebida = bebida.querySelector("h2").innerHTML
-    confirmacao()
+  if (bebida != null) {
+    bebida.classList.toggle("selecionado")
+    checkPar.classList.toggle("exibir")
+  }
+
+  elemento.classList.toggle("selecionado")
+  const checkmark = elemento.querySelector(".icone")
+  checkmark.classList.toggle("exibir")
+  bebida = document.querySelector(".opcoes-bebida").querySelector(".selecionado")
+  stringBebida = bebida.querySelector("h3").innerHTML
+  precoBebida = bebida.querySelector("h2").innerHTML
+  confirmacao()
 }
 
-function selecionarSobremesa(elemento)
-{
-    const checkPar = document.querySelector(".opcoes-sobremesa").querySelector(".exibir")
-    if(sobremesa != null)
+function selecionarSobremesa(elemento) {
+  const checkPar = document.querySelector(".opcoes-sobremesa").querySelector(".exibir")
+  if (sobremesa != null) {
+    sobremesa.classList.toggle("selecionado")
+    checkPar.classList.toggle("exibir")
+  }
 
-    {
-        sobremesa.classList.toggle("selecionado")
-        checkPar.classList.toggle("exibir")
-    }
-
-    elemento.classList.toggle("selecionado")
-    const checkmark = elemento.querySelector(".icone")
-    checkmark.classList.toggle("exibir")
-    sobremesa = document.querySelector(".opcoes-sobremesa").querySelector(".selecionado")
-    stringSobremesa = sobremesa.querySelector("h3").innerHTML
-    precoSobremesa = sobremesa.querySelector("h2").innerHTML
-    confirmacao()
+  elemento.classList.toggle("selecionado")
+  const checkmark = elemento.querySelector(".icone")
+  checkmark.classList.toggle("exibir")
+  sobremesa = document.querySelector(".opcoes-sobremesa").querySelector(".selecionado")
+  stringSobremesa = sobremesa.querySelector("h3").innerHTML
+  precoSobremesa = sobremesa.querySelector("h2").innerHTML
+  confirmacao()
 }
 
-function confirmacao()
-{
-    const botaoFecharPedido = document.querySelector(".botao-fecha-pedido")
-    if(prato != null && bebida != null && sobremesa != null)
-    {
-        botaoFecharPedido.disabled = false
-        botaoFecharPedido.classList.add("confirmar")
-        botaoFecharPedido.querySelector("h2").innerHTML = `Fechar Pedido`
-    }
+function confirmacao() {
+  const botaoFecharPedido = document.querySelector(".botao-fecha-pedido")
+  if (prato != null && bebida != null && sobremesa != null) {
+    botaoFecharPedido.disabled = false
+    botaoFecharPedido.classList.add("confirmar")
+    botaoFecharPedido.querySelector("h2").innerHTML = `Fechar Pedido`
+  }
 }
 
-function fecharPedido()
-{
-    document.querySelector(".pedido-prato").innerHTML = `${stringPrato}`
-    document.querySelector(".preco-prato").innerHTML = `${precoPrato}`
-    document.querySelector(".pedido-bebida").innerHTML = `${stringBebida}`
-    document.querySelector(".preco-bebida").innerHTML = `${precoBebida}`
-    document.querySelector(".pedido-sobremesa").innerHTML = `${stringSobremesa}`
-    document.querySelector(".preco-sobremesa").innerHTML = `${precoSobremesa}`
-    precoTotal = Number(precoPrato.replace("R$ ", "").replace(",", ".")) + Number(precoBebida.replace("R$ ", "").replace(",", ".")) + Number(precoSobremesa.replace("R$ ", "").replace(",", "."))
-    precoTotal = parseFloat(precoTotal).toFixed(2)
-    document.querySelector(".preco-total").innerHTML = `R$ ${precoTotal}`
-    document.querySelector(".confirmacao").classList.add("exibir-confirmacao")
+function fecharPedido() {
+  document.querySelector(".pedido-prato").innerHTML = `${stringPrato}`
+  document.querySelector(".preco-prato").innerHTML = `${precoPrato}`
+  document.querySelector(".pedido-bebida").innerHTML = `${stringBebida}`
+  document.querySelector(".preco-bebida").innerHTML = `${precoBebida}`
+  document.querySelector(".pedido-sobremesa").innerHTML = `${stringSobremesa}`
+  document.querySelector(".preco-sobremesa").innerHTML = `${precoSobremesa}`
+  precoTotal = Number(precoPrato.replace("R$ ", "").replace(",", ".")) + Number(precoBebida.replace("R$ ", "").replace(",", ".")) + Number(precoSobremesa.replace("R$ ", "").replace(",", "."))
+  precoTotal = parseFloat(precoTotal).toFixed(2)
+  document.querySelector(".preco-total").innerHTML = `R$ ${precoTotal}`
+  document.querySelector(".confirmacao").classList.add("exibir-confirmacao")
 }
 
-function confirmarPedido()
-{
-    const nome = prompt("Por favor, insira o seu primeiro nome:")
-    const endereco = prompt("Por favor, coloque seu endereço de entrega:")
+function confirmarPedido() {
+  const nome = prompt("Por favor, insira o seu primeiro nome:")
+  const endereco = prompt("Por favor, coloque seu endereço de entrega:")
+  if (!nome || !endereco) {
+    alert("Você precisa informar os campos solicitados!")
+  } else {
     let mensagem = `Olá, gostaria de fazer o pedido:
 - Prato: ${stringPrato}
 - Bebida: ${stringBebida}
@@ -99,10 +91,10 @@ Nome: ${nome}
 Endereço de entrega: ${endereco}`
 
     mensagem = window.encodeURIComponent(mensagem)
-    window.open(`https://wa.me/5511988783536?text=${mensagem}`)
+    window.open(`https://wa.me/5542984223536?text=${mensagem}`)
+  }
 }
 
-function cancelarPedido()
-{
-    document.querySelector(".confirmacao").classList.remove("exibir-confirmacao")
+function cancelarPedido() {
+  document.querySelector(".confirmacao").classList.remove("exibir-confirmacao")
 }
